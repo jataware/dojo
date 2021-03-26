2909333b7de439edb85f83ebb2c0d5f7493a2a44
Scott Haleen <scott.haleen@gmail.com>
2021-03-26 09:47:36 -0400

    build scripts

---
+++ build-cato.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+DT=$(date +"%Y%m%d")
+GIT=${DT}.git.$(git rev-parse --short HEAD)
+PROJECT="cato"
+VERSION="1.0.0"
+TAG="${PROJECT}_${VERSION}"
+
+GROUP=jataware
+NAME=clouseau
+IMAGE="${GROUP}/${NAME}"
+
+docker build -f cato/Dockerfile \
+       -t "${IMAGE}:${PROJECT}-dev" \
+       -t "${IMAGE}:${TAG}" \
+       -t "${IMAGE}:${TAG}-dev" \
+       -t "${IMAGE}:${GIT}" \
+       .
