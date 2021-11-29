2a2326de7cc679027e2f341bfefa51f13b21cb65
Matthew Printz <matthew@hipokrit.com>
2021-11-29 10:26:50 -0600

    Styling changes to make less ugly

--- ui/client/runlogs.js
+++ ui/client/runlogs.js
@@ -1,42 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
-import Fab from '@material-ui/core/Fab';
-import Grid from '@material-ui/core/Grid';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Typography from '@material-ui/core/Typography';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-import { Link, useHistory, useLocation, useParams } from 'react-router-dom';
-
-import BasicAlert from './components/BasicAlert';
-import DeletionDialog from './components/DeletionDialog';
-import DirectiveBox from './components/DirectiveBox';
-import FileCardList from './components/FileCardList';
-import FullScreenDialog from './components/FullScreenDialog';
-import LoadingOverlay from './components/LoadingOverlay';
+import { useParams } from 'react-router-dom';
 
 import {
-    useRunLogs
+  useRunLogs
 } from './components/SWRHooks';
-import { mergeClasses } from '@material-ui/styles';
-import { red } from '@material-ui/core/colors';
 
+const useStyles = makeStyles(() => ({
+  title: {
+    marginTop: '1em',
+  },
 
-const useStyles = makeStyles((theme) => ({
+  button: {
+    marginLeft: '2em',
+  },
+
   task: {
-    border: "solid 1px black rounded",
+    border: 'solid 1px black rounded',
   },
 
   logs: {
-    backgroundColor: theme.palette.grey[200],
+    marginTop: '1em',
+    borderRadius: '1em',
     whiteSpace: 'pre-wrap',
-    padding: '1em',
+    padding: '2em',
   },
 
   collapsed: {
@@ -47,60 +42,70 @@ const useStyles = makeStyles((theme) => ({
   notfound: {
     color: 'red',
     textDecoration: 'underline',
-  }
+  },
+
+  success: {
+    backgroundColor: 'rgba(129, 199, 132, 0.25)',
+  },
+
+  failed: {
+    backgroundColor: 'rgba(229, 115, 115, 0.25)',
+  },
+
+  running: {
+    backgroundColor: 'rgba(100, 181, 246, 0.25)',
+  },
 }));
 
-const LogStep = ({taskName, content, isOpen}) => {
-  const [expanded, setExpanded] = useState(isOpen);
+const LogStep = ({ taskName, content, state }) => {
+  const [expanded, setExpanded] = useState(state !== 'success');
   const classes = useStyles();
 
   return (
     <div className={classes.task}>
-      <h1>{taskName}</h1>
-      {
-        expanded ? 
-        <div className={classes.logs} onClick={() => (setExpanded(!expanded))}>
-          {content}
-        </div>
-        :
-        <div className={classes.collapsed} onClick={() => (setExpanded(!expanded))}>
-          Click to expand
-        </div>
-      }
+      <Typography variant="h3" className={classes.title}>{taskName}
+        <Button startIcon={expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />} variant="outlined" className={classes.button} size="small" onClick={() => (setExpanded(!expanded))}>
+          { expanded
+            ? 'Collapse'
+            : 'Expand'}
+        </Button>
+      </Typography>
+      { expanded
+        ? (
+          <div className={`${classes.logs} ${classes[state]}`}>
+            {content}
+          </div>
+        )
+        : ''}
     </div>
   );
-}
-
+};
 
 const RunLogs = () => {
   const { runid } = useParams();
   const {
-    runLogs, runLogsLoading, runLogsError, mutateRunLogs
+    runLogs, runLogsLoading, runLogsError
   } = useRunLogs(runid);
   const classes = useStyles();
 
-
-  console.log(runid, runLogs, runLogsError);
-
   return (
     <Container>
-      <h1>Logs for run {runid}</h1>
+      <Typography variant="h2">Logs for run {runid}</Typography>
       {!runLogsLoading && !runLogsError && runLogs?.tasks.map((task) => (
-        <LogStep 
+        <LogStep
           key={task.task}
-          taskName={task.task} 
-          content={task.logs} 
-          isOpen={task.state !== 'success'} 
+          taskName={task.task}
+          content={task.logs}
+          state={task.state}
         />
       ))}
 
-      {runLogsError?.status == 404 && (
+      {runLogsError?.status === 404 && (
         <div>
           <h1 className={classes.notfound}>No logs found associated with this run.</h1>
           <div>Are you sure this ran?</div>
         </div>
-      )
-      }
+      )}
     </Container>
   );
 };
