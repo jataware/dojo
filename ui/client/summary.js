1bf438e1dfb88b0c6343a68236792e5d3dd4c841
adamgilg <adam.gilg@gmail.com>
2021-12-27 13:17:58 -0500

    Summary page refactor: model details (#148)
    * Breaks model details into their own component
    
    * Removes outdated comment

--- ui/client/summary.js
+++ ui/client/summary.js
@@ -31,6 +31,7 @@ import ShorthandEditor from './components/ShorthandEditor';
 import SimpleEditor from './components/SimpleEditor';
 import SummaryAccessories from './components/SummaryAccessories';
 import SummaryIntroDialog from './components/SummaryIntroDialog';
+import SummaryModelDetails from './components/SummaryModelDetails';
 
 import {
   useConfigs, useContainerWithWorker, useDirective, useModel, useOutputFiles
@@ -76,12 +77,6 @@ const useStyles = makeStyles((theme) => ({
     right: 0,
     bottom: '2px',
     zIndex: 10
-  },
-  subsection: {
-    marginLeft: theme.spacing(1),
-  },
-  modelHeader: {
-    fontWeight: 'bold',
   },
   modelEditButton: {
     float: 'right',
@@ -301,119 +296,6 @@ const Page = ({
     }
   };
 
-  const displayModelDetails = () => {
-    let parsedCoordinates = [];
-
-    if (modelLoading) {
-      return <div>Loading...</div>;
-    }
-
-    if (modelError) {
-      return <div>There was an error, please refresh the page</div>;
-    }
-
-    if (model.geography?.coordinates.length) {
-      parsedCoordinates = model.geography?.coordinates.map((coords, i, arr) => {
-        // only display the separator if we aren't at the end of the list
-        const separator = i !== arr.length - 1 ? ', ' : '';
-
-        if (!coords[0].length || !coords[1].length) return null;
-
-        return (
-          <span key={coords}>
-            {`[${coords[0].join()};${coords[1].join()}]`}
-            {separator}
-          </span>
-        );
-      });
-    }
-
-    const causemosUrl = `https://causemos.uncharted.software/#/model/${model.family_name}/model-publishing-experiment?datacube_id=${model.id}`;
-
-    // no need to spread the following out onto a million lines
-    /* eslint-disable react/jsx-one-expression-per-line */
-    return (
-      <div>
-        <Typography variant="subtitle2" className={classes.modelHeader}>
-          Overview:
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Model Name: {model.name}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Model Website: {model.maintainer?.website}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Model Family: {model.family_name}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Model Description: {model.description}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Model Image: {model.image}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Model Start Date: {new Date(model.period?.gte).toLocaleDateString()}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Model End Date: {new Date(model.period?.lte).toLocaleDateString()}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Stochastic Model: {model.stochastic}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Model ID: {model.id}
-        </Typography>
-
-        {/* only show this button if we're looking at a published model and
-            we don't have a container running
-        */}
-        {(model?.is_published && !workerNode) && (
-          <Button
-            className={classes.causemosButton}
-            variant="contained"
-            href={causemosUrl}
-            disableElevation
-          >
-            View Model on Causemos
-          </Button>
-        )}
-
-        <Typography variant="subtitle2" className={classes.modelHeader}>Maintainer:</Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Name: {model.maintainer?.name}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Email: {model.maintainer?.email}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Organization: {model.maintainer?.organization}
-        </Typography>
-
-        <Typography variant="subtitle2" className={classes.modelHeader}>Geography:</Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Country: {model.geography?.country?.join(', ')}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Admin 1: {model.geography?.admin1?.join(', ')}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Admin 2: {model.geography?.admin2?.join(', ')}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Admin 3: {model.geography?.admin3?.join(', ')}
-        </Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          Coordinates: {parsedCoordinates}
-        </Typography>
-        <Typography variant="subtitle2" className={classes.modelHeader}>Categories:</Typography>
-        <Typography variant="body2" className={classes.subsection}>
-          {model.category?.join(', ')}
-        </Typography>
-      </div>
-    );
-  };
-
   const handleRunCommandClick = () => {
     setShorthandContents({
       editor_content: directive?.command_raw,
@@ -598,7 +480,7 @@ const Page = ({
               >
                 Edit
               </Button>
-              {displayModelDetails()}
+              <SummaryModelDetails model={model} />
             </div>
           </Grid>
         </Grid>
@@ -616,7 +498,7 @@ const Page = ({
             >
               {model.is_published ? 'Create New Model Version' : 'Edit Model'}
             </Fab>
-          ): <></>}
+          ) : <></>}
           {!model.is_published ? (
             <Tooltip
               title={
