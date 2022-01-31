32e60397bdda83b7a6c924df32002ed1f3e815d1
Matthew Printz <matthew@hipokrit.com>
2022-01-31 10:07:19 -0600

    Bump version: 2.2.7 → 2.3.0

--- build-cato.sh
+++ build-cato.sh
@@ -5,7 +5,7 @@ BUILD_TIME=$(date +%FT%T%Z)
 COMMIT=$(git rev-parse --short HEAD)
 GIT=${DT}.git.${COMMIT}
 PROJECT="cato"
-VERSION="2.2.7"
+VERSION="2.3.0"
 TAG="${PROJECT}_${VERSION}"
 
 GROUP=jataware
