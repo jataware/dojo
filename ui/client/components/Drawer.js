39c0464a4dca10322530e072bf8da8e4b5144757
adamgilg <adam.gilg@gmail.com>
2022-05-27 16:05:51 -0400

    Adds MUI Drawer wrapper with confirm dialog (#206)
    * Adds a simple Drawer component that adds a few basic styles on the MUI drawer and gives it the ConfirmDialog on close
    
    * Changes ConfirmDialog body and title to be props with defaults

---
+++ ui/client/components/Drawer.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+
+import { withStyles } from '@material-ui/core/styles';
+
+import CloseIcon from '@material-ui/icons/Close';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+
+import ConfirmDialog from './ConfirmDialog';
+
+export default withStyles(({ spacing }) => ({
+  root: {
+    width: '30%',
+    minWidth: '20rem',
+    padding: spacing(3)
+  },
+  drawerControls: {
+    display: 'flex',
+    justifyContent: 'flex-end'
+  },
+}))(({
+  anchorPosition,
+  classes,
+  children,
+  confirmBody = 'Please confirm that you want to discard your changes.',
+  confirmTitle = 'Are you sure you want to discard your work?',
+  onClose,
+  open,
+}) => {
+  const [confirmClose, setConfirmClose] = useState(false);
+
+  const handleClose = () => {
+    setConfirmClose(true);
+  };
+
+  const handleConfirmedClose = () => {
+    setConfirmClose(false);
+    onClose(true);
+  };
+
+  return (
+    <>
+      <Drawer
+        variant="persistent"
+        classes={{ paper: classes.root }}
+        anchor={anchorPosition}
+        open={open}
+        onClose={handleClose}
+      >
+        <>
+          <div className={classes.drawerControls}>
+            <IconButton onClick={handleClose}>
+              <CloseIcon />
+            </IconButton>
+          </div>
+          {children}
+        </>
+      </Drawer>
+
+      {/* unmount the dialog so we reset its state entirely
+        otherwise it can get into a closing state if opened again */}
+      {confirmClose && (
+        <ConfirmDialog
+          accept={handleConfirmedClose}
+          body={confirmBody}
+          open={confirmClose}
+          reject={() => setConfirmClose(false)}
+          title={confirmTitle}
+        />
+      )}
+    </>
+  );
+});
