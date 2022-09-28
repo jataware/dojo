3cf5864cabae74706805bb44553a5c7e250705d1
Matthew Printz <matt@jataware.com>
2022-09-28 09:18:28 -0500

    Reset phantom

--- phantom/ui/client/index.js
+++ phantom/ui/client/index.js
@@ -1,16 +1,13 @@
-import React, { useContext, createContext, useState } from 'react';
+import React from 'react';
 
 import './style.css';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
-import KeyCloak from 'keycloak-js';
 import ReactDOM from 'react-dom';
-import { ReactKeycloakProvider, useKeycloak } from "@react-keycloak/web";
 import {
   Route,
   BrowserRouter as Router,
   Switch,
-  Redirect,
 } from 'react-router-dom';
 
 import { ThemeProvider } from '@material-ui/core/styles';
@@ -38,56 +35,31 @@ import DatasetRegistrationStepper from './datasets/RegistrationStepper';
 import DatasetPreview from './datasets/Preview';
 import DatasetAnnotate from './datasets/Annotate';
 
-
-const auth_base = 'http://localhost:8079';
-const keycloak = new KeyCloak({
-  url: auth_base,
-  realm: 'Uncharted',
-  clientId: 'causemos',
-});
-
-
 export default function Main() {
   return (
-    <ReactKeycloakProvider authClient={keycloak}>
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route component={LandingPage} exact path="/" />
-          <ProtectedRoute component={Model} exact path="/model" />
-          <ProtectedRoute component={ViewModels} exact path="/models" />
-          <ProtectedRoute component={ViewDatasets} exact path="/datasets" />
-          <ProtectedRoute component={DatasetAnnotate} exact path="/datasets/annotate" />
-          <ProtectedRoute component={DatasetPreview} exact path="/datasets/preview" />
-          <ProtectedRoute component={DatasetRegistrationStepper} path="/datasets/:flowslug/:step?/:datasetId?" />
-          <ProtectedRoute component={Provision} exact path="/provision/:modelId" />
-          <ProtectedRoute component={Provisioning} exact path="/provisioning/:modelId" />
-          <ProtectedRoute component={Terminal} exact path="/term/:modelid" />
-          <ProtectedRoute component={Summary} exact path="/summary/:modelId" />
-          <ProtectedRoute component={DatasetSummary} exact path="/dataset_summary" />
-          <ProtectedRoute component={Admin} exact path="/admin" />
-          <ProtectedRoute component={ViewRuns} exact path="/runs" />
-          <ProtectedRoute component={RunSummary} exact path="/runs/:runid" />
-          <ProtectedRoute component={RunLogs} exact path="/runlogs/:runid" />
-          <Route path="/*" render={() => <h2>404 Not Found</h2>} />
-        </Switch>
-      </Router>
-    </ReactKeycloakProvider>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route component={LandingPage} exact path="/" />
+        <Route component={Model} exact path="/model" />
+        <Route component={ViewModels} exact path="/models" />
+        <Route component={ViewDatasets} exact path="/datasets" />
+        <Route component={DatasetAnnotate} exact path="/datasets/annotate" />
+        <Route component={DatasetPreview} exact path="/datasets/preview" />
+        <Route component={DatasetRegistrationStepper} path="/datasets/:flowslug/:step?/:datasetId?" />
+        <Route component={Provision} exact path="/provision/:modelId" />
+        <Route component={Provisioning} exact path="/provisioning/:modelId" />
+        <Route component={Terminal} exact path="/term/:modelid" />
+        <Route component={Summary} exact path="/summary/:modelId" />
+        <Route component={DatasetSummary} exact path="/dataset_summary" />
+        <Route component={Admin} exact path="/admin" />
+        <Route component={ViewRuns} exact path="/runs" />
+        <Route component={RunSummary} exact path="/runs/:runid" />
+        <Route component={RunLogs} exact path="/runlogs/:runid" />
+        <Route path="/*" render={() => <h2>404 Not Found</h2>} />
+      </Switch>
+    </Router>
   );
-}
-
-function ProtectedRoute({ children, ...props }) {
-  const { keycloak } = useKeycloak();
-  console.log(keycloak);
-  const isLoggedIn = keycloak.authenticated;
-  if (isLoggedIn) {
-    return <Route {...props} render={ () => children }/>
-  }
-  else {
-    keycloak.login();
-    return <h1>Redirecting to login</h1>
-    // return <Redirect to="/"/>
-  }
 }
 
 ReactDOM.render(
@@ -99,6 +71,5 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById('app')
 );
-
 
 module.hot.accept();
