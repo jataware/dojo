77d15654a02fa93827b399f644038a6339537b9e
Adam Gilgoff <adam.gilg@gmail.com>
2023-01-31 18:31:51 -0500

    Cleans up unnecessary geoman buttons, adds event listeners to hook drawings up to react state

--- ui/client/datasets/ClipMap.js
+++ ui/client/datasets/ClipMap.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
@@ -13,25 +13,65 @@ import {
   TileLayer,
 } from 'react-leaflet';
 
-const Geoman = () => {
+const Geoman = ({ setDrawings }) => {
   const context = useLeafletContext();
 
   useEffect(() => {
     const leafletContainer = context.layerContainer || context.map;
 
+    const setNewDrawings = () => {
+      // just extract the lat/lng coords
+      const drawingsCoords = leafletContainer.pm.getGeomanDrawLayers()
+        .map((drawing) => drawing._latlngs[0]);
+
+      setDrawings(drawingsCoords);
+    };
+
     if (leafletContainer) {
+      // when a drawing is removed
+      leafletContainer.on('pm:remove', () => {
+        setNewDrawings();
+      });
+
+      // every time we leave drag mode
+      leafletContainer.on('pm:globaldragmodetoggled', ({ enabled }) => {
+        if (!enabled) setNewDrawings();
+      });
+
+      // every time we leave edit mode
+      leafletContainer.on('pm:globaleditmodetoggled', ({ enabled }) => {
+        if (!enabled) setNewDrawings();
+      });
+
+      // every time a drawing is created
+      leafletContainer.on('pm:create', () => {
+        setNewDrawings();
+      });
+
       leafletContainer.pm.addControls({
         position: 'topleft',
         drawCircle: false,
+        drawMarker: false,
+        drawRectangle: false,
+        drawCircleMarker: false,
+        drawText: false,
+        drawPolyline: false,
+        cutPolygon: false,
+        rotateMode: false,
       });
     }
-  }, [context]);
+    // TODO: clean up leafletContainer event listeners?
+    return () => {
+      leafletContainer.pm.removeControls();
+    };
+  }, [context, setDrawings]);
 
   return null;
 };
 
 export default withStyles((theme) => ({
 }))(({}) => {
+  const [drawings, setDrawings] = useState([]);
 
   return (
     <div>
@@ -42,12 +82,17 @@ export default withStyles((theme) => ({
         scrollWheelZoom={false}
         style={{ height: 340, margin: '0 auto' }}
       >
-        <Geoman />
+        <Geoman setDrawings={setDrawings} />
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
       </MapContainer>
+      {drawings.map((drawing) => (
+        <Typography variant="h6" key={`${drawing[0].lat}${drawing[0].lng}`}>
+          Here is a drawing: {JSON.stringify(drawing)}
+        </Typography>
+      ))}
     </div>
   );
 });
