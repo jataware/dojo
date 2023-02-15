21702e0f339385e42a80c44401402427500101eb
Joel Quiles <joel@radcoding.com>
2023-02-15 15:21:21 -0500

    Added stopwords disallowe list to semantic search highlighting for document explorer

--- ui/client/documents/utils.js
+++ ui/client/documents/utils.js
@@ -4,6 +4,9 @@ import mapKeys from 'lodash/mapKeys';
 import reduce from 'lodash/reduce';
 import flow from 'lodash/flow';
 
+// Includes English stopwords that don't add any semantic meaning to matches:
+const DISALLOWED_HIGHLIGHTS = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than'];
+
 /**
  * `highlightText` helper. Returns raw data of which words should be highlighted.
  * Easier for testing (see unit test).
@@ -19,10 +22,17 @@ export function calculateHighlightTargets(text, highlights) {
         .split(" ")
         .map(i => i.trim());
 
-  const highlighted = parts.map((part) => ({
-    text: part,
-    highlight: highlightWords.includes(part && part.toLowerCase().trim())
-  }));
+  const highlighted = parts.map((part) => {
+
+    const normalizedPart = part && part.toLowerCase().trim();
+    const isDisallowed = DISALLOWED_HIGHLIGHTS.includes(normalizedPart);
+
+    return {
+      text: part,
+      highlight: !isDisallowed && highlightWords.includes(normalizedPart)
+    };
+
+  });
 
   return highlighted;
 }
