8fd1df6290345bcd111c2288c302004c7a4e0860
marshHawk4 <kyle@jataware.com>
2023-02-23 17:02:23 -0500

    working

--- api/src/utils.py
+++ api/src/utils.py
@@ -5,6 +5,7 @@ import csv
 from urllib.parse import urlparse
 from io import BytesIO, StringIO
 from zlib import compressobj
+from fastapi.logger import logger
 
 import pandas as pd
 from elasticsearch import Elasticsearch
@@ -15,10 +16,39 @@ from src.settings import settings
 from validation import ModelSchema
 
 es = Elasticsearch([settings.ELASTICSEARCH_URL], port=settings.ELASTICSEARCH_PORT)
+s3_client = boto3.client("s3")
+
+storage_host=os.getenv("STORAGE_HOST")
+if "minio" in storage_host:
+    s3 = boto3.resource(
+        "s3",
+        endpoint_url=os.getenv("STORAGE_HOST"),
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        aws_session_token=None,
+        config=boto3.session.Config(signature_version="s3v4"),
+        verify=False,
+    )
+else:
+    s3 = boto3.resource(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_session_token=None,
+    config=boto3.session.Config(signature_version="s3v4"),
+    verify=False,
+)
 
 # S3 OBJECT
-s3 = boto3.client("s3")
-
+s3 = boto3.resource(
+    "s3",
+    endpoint_url=os.getenv("STORAGE_HOST"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_session_token=None,
+    config=boto3.session.Config(signature_version="s3v4"),
+    verify=False,
+)
 
 def try_parse_int(s: str, default: int = 0) -> int:
     try:
@@ -109,27 +139,46 @@ def run_model_with_defaults(model_id):
 
 
 def get_rawfile(path):
+    """Gets a file from a filepath
+
+    Args:
+        path (str): URI to file
+
+    Raises:
+        FileNotFoundError: If the file cannnot be found on S3.
+        RuntimeError: If the path URI does not begin with 'file' or 's3'
+        there is no handler for it yet.
+
+    Returns:
+        file: a file-like object
+    """
     location_info = urlparse(path)
 
     if location_info.scheme.lower() == "file":
-        raw_file = open(location_info.path, "rb")
-    elif location_info.scheme.lower() == "s3":
+        return open(location_info.path, "rb")
+    if location_info.scheme.lower() in ["s3", "minio"]:
         try:
             file_path = location_info.path.lstrip("/")
             raw_file = tempfile.TemporaryFile()
-            s3.download_fileobj(
-                Bucket=location_info.netloc, Key=file_path, Fileobj=raw_file
-            )
+            s3.Object(location_info.netloc, file_path).download_fileobj(raw_file)
             raw_file.seek(0)
-        except botocore.exceptions.ClientError as e:
-            raise FileNotFoundError()
+        except botocore.exceptions.ClientError as error:
+            raise FileNotFoundError() from error
     else:
         raise RuntimeError("File storage format is unknown")
 
     return raw_file
 
-
 def put_rawfile(path, fileobj):
+    """Puts/uploads a file at URI specified
+    Args:
+        path (str): URI to put/upload the file to.
+        fileobj (file): The file-like object to upload.
+    Raises:
+        RuntimeError: If the path URI does not begin with 'file' or 's3'
+        there is no handler for it yet.
+    """
+
     location_info = urlparse(path)
 
     if location_info.scheme.lower() == "file":
@@ -137,9 +186,9 @@ def put_rawfile(path, fileobj):
             os.makedirs(os.path.dirname(location_info.path), exist_ok=True)
         with open(location_info.path, "wb") as output_file:
             output_file.write(fileobj.read())
-    elif location_info.scheme.lower() == "s3":
+    elif location_info.scheme.lower() in ["s3", "minio"]:
         output_path = location_info.path.lstrip("/")
-        s3.put_object(Bucket=location_info.netloc, Key=output_path, Body=fileobj)
+        s3.Object(location_info.netloc, output_path).put(Body=fileobj)
     else:
         raise RuntimeError("File storage format is unknown")
 
@@ -149,7 +198,7 @@ def list_files(path):
     if location_info.scheme.lower() == "file":
         return os.listdir(location_info.path)
     elif location_info.scheme.lower() == "s3":
-        s3_list = s3.list_objects(
+        s3_list = s3_client.list_objects(
             Bucket=location_info.netloc, Marker=location_info.path
         )
         s3_contents = s3_list["Contents"]
