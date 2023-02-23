ff7f1b6e4849bf18de870350384657695227a1bb
marshHawk4 <kyle@jataware.com>
2023-02-23 17:04:18 -0500

    cleaned

--- api/src/utils.py
+++ api/src/utils.py
@@ -39,16 +39,6 @@ else:
     verify=False,
 )
 
-# S3 OBJECT
-s3 = boto3.resource(
-    "s3",
-    endpoint_url=os.getenv("STORAGE_HOST"),
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    aws_session_token=None,
-    config=boto3.session.Config(signature_version="s3v4"),
-    verify=False,
-)
 
 def try_parse_int(s: str, default: int = 0) -> int:
     try:
