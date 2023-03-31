94fc1d39f879f8f3671bb9ed4a65b0c921c521a5
Joel Quiles <joel@radcoding.com>
2023-03-31 15:27:42 -0400

    Implemented highlight flag for paragraphs semantic search (#52)
    * Implemented highlight flag for paragraphs semantic search, which makes it so
    that it includes highlights in response upon request (off by default). Added
    better schemas for all response models on Documents API.
    
    * Updated schema to be clearer on when a document ID should be sent on the
    request, as well as removed allowExtra attributes on Document model.

--- api/src/documents.py
+++ api/src/documents.py
@@ -106,7 +106,7 @@ class HighlightData(BaseModel):
     matches: list[str]
 
 class HighlightResponseModel(BaseModel):
-    highlights: List[List[Dict[str,Any]]]
+    highlights: List[List[DocumentSchema.Highlight]]
 
 @router.post(
     "/paragraphs/highlight", response_model=HighlightResponseModel
@@ -127,6 +127,7 @@ def semantic_highlight_paragraphs(payload: HighlightData):
 )
 def semantic_search_paragraphs(query: str,
                                scroll_id: Optional[str]=None,
+                               highlight: Optional[bool]=False,
                                size: int = 10):
     """
     Uses query to perform a Semantic Search on paragraphs; where LLM embeddings
@@ -182,7 +183,13 @@ def semantic_search_paragraphs(query: str,
                             scroll="2m",
                             size=size)
 
-    result_len = len(results["hits"]["hits"])
+    hits = results["hits"]["hits"]
+
+    if highlight:
+        text_vec = list(map(lambda x: x.get('_source').get('text'), hits))
+        highlights = highlighter.highlight_multiple(query, text_vec)
+
+    result_len = len(hits)
 
     if result_len < size:
         scroll_id = None
@@ -191,25 +198,29 @@ def semantic_search_paragraphs(query: str,
 
     max_score = results["hits"]["max_score"]
 
-    def formatOneResult(r):
+    def formatOneResult(r, index):
         r["_source"]["metadata"] = {}
         r["_source"]["metadata"]["match_score"] = r["_score"]
         r["_source"]["id"] = r["_id"]
+
+        if highlight:
+            r["_source"]["highlights"] = highlights[index]
+
         return r["_source"]
 
     return {
         "hits": results["hits"]["total"]["value"],
         "items_in_page": result_len,
         "max_score": max_score,
-        "results": [formatOneResult(i) for i in results["hits"]["hits"]],
+        "results": [formatOneResult(item, index) for index, item in enumerate(hits)],
         "scroll_id": scroll_id
     }
 
 
 @router.get(
-    "/paragraphs/{paragraph_id}"
+    "/paragraphs/{paragraph_id}", response_model=DocumentSchema.Paragraph
 )
-def get_paragraph(paragraph_id: str) -> DocumentSchema.Paragraph:
+def get_paragraph(paragraph_id: str):
     """
     """
     try:
@@ -340,7 +351,7 @@ def list_documents(scroll_id: Optional[str]=None, size: int = 10):
 
 
 @router.get(
-    "/documents/{document_id}/paragraphs"
+    "/documents/{document_id}/paragraphs", response_model=DocumentSchema.DocumentTextResponse
 )
 def get_document_text(document_id: str,
                       scroll_id: Optional[str] = None,
@@ -382,7 +393,7 @@ def get_document_text(document_id: str,
 
 
 @router.get(
-    "/documents/{document_id}"
+    "/documents/{document_id}", response_model=DocumentSchema.Model
 )
 def get_document(document_id: str) -> DocumentSchema.Model:
     """
@@ -397,7 +408,7 @@ def get_document(document_id: str) -> DocumentSchema.Model:
 
 
 @router.post("/documents")
-def create_document(payload: DocumentSchema.Model):
+def create_document(payload: DocumentSchema.CreateModel):
     """
     Saves a document [metadata] into elasticsearch
     """
@@ -410,7 +421,6 @@ def create_document(payload: DocumentSchema.Model):
     except KeyError:
         pass
 
-    # TODO verify if necessary to use dictionary or to_json
     body = json.dumps(payload_dict)
 
     es.index(index="documents", body=body, id=document_id)
@@ -427,21 +437,24 @@ def create_document(payload: DocumentSchema.Model):
 
 
 @router.patch("/documents/{document_id}")
-def update_document(payload: DocumentSchema.Model, document_id: str):
+def update_document(payload: DocumentSchema.CreateModel, document_id: str):
     """
     Partially updates a document. Accepts a full object as well.
     """
 
     payload_dict = payload.dict(exclude_unset=True)
 
-    logger.info(f"update payload: {payload_dict}")
-
-    try:  # Delete id if present, else ignore.
-        del payload_dict["id"]
-    except KeyError:
-        pass
-
-    es.update(index="documents", body={"doc": payload_dict}, id=document_id)
+    if payload_dict:
+        es.update(index="documents", body={"doc": payload_dict}, id=document_id)
+    else:
+        return Response(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            headers={
+                "location": f"/api/documents/{document_id}",
+                "content-type": "application/json",
+            },
+            content=json.dumps({"error": f"Could not update Document with id={document_id}, invalid or unknown update attributes were provided"})
+        )
 
     return Response(
         status_code=status.HTTP_200_OK,
@@ -449,7 +462,7 @@ def update_document(payload: DocumentSchema.Model, document_id: str):
             "location": f"/api/documents/{document_id}",
             "content-type": "application/json",
         },
-        content={"result": f"Updated document with id = {document_id}"}
+        content=json.dumps({"result": f"Updated document with id = {document_id}"})
     )
 
 
@@ -482,8 +495,6 @@ def upload_file(
     """
     original_filename = file.filename
     _, ext = os.path.splitext(original_filename)
-
-    logger.info(f"settings.DOCUMENT_STORAGE_BASE_URL: {settings.DOCUMENT_STORAGE_BASE_URL}")
 
     dest_path = os.path.join(settings.DOCUMENT_STORAGE_BASE_URL, f"{document_id}-{original_filename}")
     put_rawfile(path=dest_path, fileobj=file.file)
