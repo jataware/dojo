d9f193e5ccd9e379606b665714e529a0d77bf9b6
Joel Quiles <joel@radcoding.com>
2023-05-10 16:37:36 -0400

    Cleaned conftest for clarity and essentials.

--- api/conftest.py
+++ api/conftest.py
@@ -1,7 +1,5 @@
-# import os
 import sys
-# import pytest
 from pathlib import Path
 
-# Add this folder to pythonpath so that test can see all modules (under src/*)
+# Adding api/ to pythonpath so that test can see all modules within (eg src.*)
 sys.path.append(str(Path(__file__).resolve().parent))
