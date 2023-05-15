a1053992bc7c44cc100e46bbf7dcd3dd1950bc5f
adamgilg <adam.gilg@gmail.com>
2023-05-15 14:42:47 -0400

    Delete files in Document Uploader (#68)
    * Adds in functioning delete buttons on document upload file list files (file metadata editing/upload queue)
    
    * Defaults selectedFileIndex in documents/upload/index to 0 and removes null checks, changes a check for null fileIndex for empty files array

--- ui/client/documents/upload/index.js
+++ ui/client/documents/upload/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import isEmpty from 'lodash/isEmpty';
@@ -6,16 +6,13 @@ import snakeCase from 'lodash/snakeCase';
 
 import { PDFDocument } from 'pdf-lib';
 
-// import ClearIcon from '@material-ui/icons/Clear';
 import Alert from '@material-ui/lab/Alert';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import LinearProgress from '@material-ui/core/LinearProgress';
 import Typography from '@material-ui/core/Typography';
 
 import { withStyles } from '@material-ui/core/styles';
-// import get from 'lodash/get';
 
 import { readFile } from '../utils';
 
@@ -148,7 +145,7 @@ const UploadDocumentForm = withStyles((theme) => ({
 
   const [files, setFiles] = useState([]);
   const [allPDFMetadata, setAllPDFMetadata] = useState([]);
-  const [selectedFileIndex, setSelectedFileIndex] = useState(null);
+  const [selectedFileIndex, setSelectedFileIndex] = useState(0);
 
   const [loading, setLoading] = useState(false);
   const [uploading, setUploading] = useState(false);
@@ -156,7 +153,7 @@ const UploadDocumentForm = withStyles((theme) => ({
   const [acceptedFilesCount, setAcceptedFilesCount] = useState(0);
   const [acceptedFilesParsed, setAcceptedFilesParsed] = useState(0);
 
-  const selectedFile = selectedFileIndex !== null ? files[selectedFileIndex] : {};
+  const selectedFile = files[selectedFileIndex] || {};
 
   const history = useHistory();
 
@@ -196,8 +193,8 @@ const UploadDocumentForm = withStyles((theme) => ({
         // Let's update the state all together when we have everything available.
         // It's hard to trust and coordinate batch updates when performing updates
         // both outside and inside async promise handler:
-        setAllPDFMetadata(prevMetadata => [ ...prevMetadata, ...allPdfData ]);
-        setFiles(prevFiles => [ ...prevFiles, ...formattedFiles ]);
+        setAllPDFMetadata(prevMetadata => [...prevMetadata, ...allPdfData]);
+        setFiles(prevFiles => [...prevFiles, ...formattedFiles]);
         setSelectedFileIndex(selectedFileIndex => selectedFileIndex || 0);
 
         setLoading(false);
@@ -205,7 +202,6 @@ const UploadDocumentForm = withStyles((theme) => ({
         setAcceptedFilesCount(0);
         setAcceptedFilesParsed(0);
       });
-
   };
 
   /**
@@ -223,54 +219,46 @@ const UploadDocumentForm = withStyles((theme) => ({
   };
 
   const submitAndUploadDocuments = () => {
-
     setUploading(true);
-
     files.forEach((file, idx) => {
-
       // We use the parsed doc Date object type until the very last minute
       // so that the UI calendar widget can work properly, and then we
       // format for the server before submitting.
-      const metadataClone = {...allPDFMetadata[idx]};
+      const metadataClone = { ...allPDFMetadata[idx] };
       metadataClone.creation_date = formatDate(metadataClone.creation_date);
 
       const documentsPromise = axios({
         method: 'post',
-        url: `/api/dojo/documents`,
+        url: '/api/dojo/documents',
         data: metadataClone,
         params: {}
       }).catch((e) => {
         console.log('Error creating doc', e);
-      }).then(response => {
+      }).then((response) => {
         const doc = response.data;
         return uploadFile(file, `/api/dojo/documents/${doc.id}/upload`, {});
       }).catch((e) => {
-        console.log("Error uploading files", e);
+        console.log('Error uploading files', e);
       }).then(() => {
         history.push('/documents');
       });
-
-      // if we map files to promises to await all uploads:
-      // return documentsPromise;
-
     });
   };
 
-  /**
-   * TODO: In order to handle deletes- either do backflips around deleted indexes,
-   * not rendering deleted ones, etc, or change the collection/representation
-   * of data from arrays and selected index to an object with key=>properties.
-   **/
   const handleFileDelete = (index) => {
-    // TODO Implement me once we use SortedMap
-    console.log("deleting file index:", index);
+    // always reset back to zero, even if we aren't touching the selectedFile
+    // so we don't end up in a weird state where we're modifying the array around this index
+    setSelectedFileIndex(0);
+    // remove the file at the index from both the metadata and files arrays
+    setAllPDFMetadata((prev) => [...prev.slice(0, index), ...prev.slice(index + 1)]);
+    setFiles((prev) => [...prev.slice(0, index), ...prev.slice(index + 1)]);
   };
 
   return (
     <Container
       className={classes.root}
       component="main"
-      maxWidth={selectedFileIndex === null ? "md" : false}
+      maxWidth={isEmpty(files) ? 'md' : false}
     >
       <Typography
         variant="h3"
@@ -290,7 +278,7 @@ const UploadDocumentForm = withStyles((theme) => ({
 
       <div className={classes.mainContent}>
         <Alert
-          style={{border: "none"}}
+          style={{ border: 'none' }}
           severity="info"
           variant="outlined"
         >
@@ -305,7 +293,7 @@ const UploadDocumentForm = withStyles((theme) => ({
         {loading && (
           <div>
             <br />
-            <div style={{display: 'flex', alignItems: 'center'}}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
               <CustomLoading
                 variant="determinate"
                 value={(acceptedFilesParsed / acceptedFilesCount) * 100}
@@ -320,22 +308,19 @@ const UploadDocumentForm = withStyles((theme) => ({
         )}
 
         {!isEmpty(files) && (
-          <div style={{
-            flex: '3 0 auto'
-          }}>
-
+          <div style={{ flex: '3 0 auto' }}>
             <Alert
-              style={{border: "none", paddingTop: '0.5rem'}}
+              style={{ border: 'none', paddingTop: '0.5rem' }}
               severity="info"
               variant="outlined"
             >
-              The following {files.length > 1 ? `${files.length} files` : "file"} will be uploaded. Confirm or edit
+              The following {files.length > 1 ? `${files.length} files` : 'file'} will be uploaded. Confirm or edit
               document metadata fields before proceeding.
             </Alert>
 
             <div className={classes.fileList}>
 
-              <section style={{flex: '4 2 400px', padding: '1rem'}}>
+              <section style={{ flex: '4 2 400px', padding: '1rem' }}>
                 <SelectedFileList
                   onDelete={handleFileDelete}
                   files={files}
@@ -344,28 +329,23 @@ const UploadDocumentForm = withStyles((theme) => ({
                 />
               </section>
 
-              {selectedFileIndex !== null && (
-                <section style={{flex: '6 2 400px'}}>
-                  <PDFViewer
-                    file={selectedFile}
-                  />
-                </section>
-              )}
+              <section style={{ flex: '6 2 400px' }}>
+                <PDFViewer
+                  file={selectedFile}
+                />
+              </section>
 
-              {selectedFileIndex !== null && (
-                <section
-                  style={{flex: '4 1 500px'}}
-                >
-                  {/* NOTE key==selectedIndex renders a form per file, but only for the file in question; */}
-                  {/*   shorthand for adding a form per file, only displaying selected file form */}
-                  <EditMetadata
-                    key={selectedFileIndex}
-                    onSave={handleDocFieldChange}
-                    filename={selectedFile.name}
-                    metadata={allPDFMetadata[selectedFileIndex]} />
-                </section>
-              )}
+              <section style={{ flex: '4 1 500px' }}>
 
+                {/* key==selectedIndex renders a form per file, but only for the file in question;
+                  shorthand for adding a form per file, only displaying selected file form */}
+                <EditMetadata
+                  key={selectedFileIndex}
+                  onSave={handleDocFieldChange}
+                  filename={selectedFile.name}
+                  metadata={allPDFMetadata[selectedFileIndex]}
+                />
+              </section>
             </div>
 
             <div className={classes.navContainer}>
@@ -379,7 +359,7 @@ const UploadDocumentForm = withStyles((theme) => ({
               &nbsp;
               {uploading ? (
                 <div>
-                  <div style={{display: 'flex', alignItems: 'center'}}>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
                     <CustomLoading
                       variant="indeterminate"
                     />
