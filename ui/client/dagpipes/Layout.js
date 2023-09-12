96eb8ce7e76e4a8f2b78c113d0193665dfd810c2
Adam Gilgoff <adam.gilg@gmail.com>
2023-09-12 17:30:41 -0400

    More cleanup

--- ui/client/dagpipes/Layout.js
+++ ui/client/dagpipes/Layout.js
@@ -2,14 +2,9 @@ import React from 'react';
 
 import { makeStyles } from 'tss-react/mui';
 
-import { useSelector, useDispatch } from 'react-redux';
-
-import capitalize from 'lodash/capitalize';
+import { useSelector } from 'react-redux';
 
 import './layout.scss';
-
-import { setEdgeType } from './dagSlice';
-import { graphEdgeTypes } from './constants';
 
 const useStyles = makeStyles()(() => ({
   main: {
@@ -29,44 +24,12 @@ const useStyles = makeStyles()(() => ({
     marginInlineEnd: 0,
     paddingInlineStart: 0,
   },
-  aside: {
-    gridArea: 'aside',
-    marginRight: '0.5rem',
-    background: '#fff',
-    borderRadius: '6px',
-    color: 'rgba(0,0,0,.87)',
-    border: '1px solid #e5e5e5ad',
-  },
   footer: {
     gridArea: 'footer',
     // border: 1px dashed green;
     padding: '1rem',
   },
 }));
-
-const edgeTypeOptions = graphEdgeTypes.map((t) => ({ value: t === 'bezier' ? 'default' : t, label: capitalize(t) }));
-
-const EdgeTypeSelector = () => {
-  const dispatch = useDispatch();
-
-  const { edgeType } = useSelector((state) => state.dag);
-
-  return (
-    <select
-      value={edgeType}
-      onChange={(e) => { dispatch(setEdgeType(e.target.value)); }}
-    >
-      {edgeTypeOptions.map((option) => (
-        <option
-          key={option.value}
-          value={option.value}
-        >
-          {option.label}
-        </option>
-      ))}
-    </select>
-  );
-};
 
 const Footer = () => {
   const { nodeCount, unsavedChanges } = useSelector((state) => state.dag);
@@ -82,39 +45,12 @@ const Footer = () => {
 };
 
 const GridLayout = ({ children }) => {
-  const { classes, css } = useStyles();
+  const { classes } = useStyles();
   return (
     <div className="container">
       <main className={classes.main}>
         {children}
       </main>
-
-      <aside className={css`background: white; padding-bottom: 1.5rem;`}>
-        <h3 className={css`
-             background: linear-gradient(60deg,#26c6da,#00acc1);
-             padding: 1.33rem 0;
-             border-top-left-radius: 5px;
-             border-top-right-radius: 5px;
-             margin-top: -1px;
-             width: 100%;
-             color: white;`}
-        >Settings
-        </h3>
-
-        {/* <h4 className={css`color: gray;`}> */}
-        {/*   Scenarios */}
-        {/* </h4> */}
-
-        {/* <div className={css`display:flex; flex-direction: column; align-items: center;`}> */}
-        {/*   <ScenarioSelection /> */}
-        {/* </div> */}
-
-        <h4 className={css`color: gray;`}>
-          Edge Type
-        </h4>
-        <EdgeTypeSelector />
-      </aside>
-
       <Footer />
 
     </div>
