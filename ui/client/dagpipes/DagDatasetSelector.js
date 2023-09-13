714d53dbefd75cf6befecb86699bad78b659939c
Adam Gilgoff <adam.gilg@gmail.com>
2023-09-13 12:46:02 -0400

    Hooks up very initial ui connection to backend ncfiles endpoint

--- ui/client/dagpipes/DagDatasetSelector.js
+++ ui/client/dagpipes/DagDatasetSelector.js
@@ -1,132 +1,32 @@
-import React, { useEffect, useState } from 'react';
-
-import axios from 'axios';
-
-import { useDispatch } from 'react-redux';
-
-import format from 'date-fns/format';
-
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import { DataGrid } from '@mui/x-data-grid';
+import React from 'react';
 
 import { useNCDatasets } from '../components/SWRHooks';
-import ExpandableDataGridCell from '../components/ExpandableDataGridCell';
-import { setSavedDatasets, nextModelerStep } from './dagSlice';
 
-const expandableCell = ({ value, colDef }) => (
-  <ExpandableDataGridCell
-    value={value}
-    width={colDef.computedWidth}
-  />
-);
-
-const columns = [
-  { field: 'name', headerName: 'Name', width: 130 },
-  {
-    field: 'id',
-    headerName: 'ID',
-    renderCell: expandableCell,
-    width: 288,
-  },
-  {
-    field: 'created_at',
-    headerName: 'Date Created',
-    valueFormatter: (params) => (
-      format(new Date(params.value), 'MM/dd/yyyy')
-    ),
-    width: 120,
-  },
-  {
-    field: 'maintainer.name',
-    headerName: 'Maintainer',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    valueGetter: (params) => params.row?.maintainer.name,
-    width: 160,
-  },
-  {
-    field: 'description',
-    headerName: 'Description',
-    renderCell: expandableCell,
-    minWidth: 200,
-  },
-  {
-    field: 'fileData.raw.url',
-    headerName: 'File Name',
-    valueGetter: (params) => params.row?.fileData.raw.url,
-    minWidth: 200,
-  },
-];
-
+// get all datasets from /indicators/ncfiles (bad endpoint name, change later)
+// let user select which datasets they want to join
+// when loading the next page, hit the annotations endpoint for those datasets to get the columns
+// /api/dojo/indicators/${datasetInfo.id}/annotations - Annotate.js - getAnnotations line 114/137
 const DagDatasetSelector = () => {
   const { datasets, datasetsLoading, datasetsError } = useNCDatasets();
-  const [selectedDatasets, setSelectedDatasets] = useState([]);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    document.title = 'Data Modeler';
-  }, []);
-
-  const handleNext = async () => {
-    const annotations = selectedDatasets.map(async (datasetId) => {
-      // get the annotations for each selected dataset so that we can get the column names
-      const datasetAnnotation = await axios.get(`/api/dojo/indicators/${datasetId}`);
-      return datasetAnnotation;
-    });
-
-    // once all our requests have returned
-    Promise.all(annotations).then((responses) => {
-      // build up an object
-      const parsedDatasets = responses.reduce((accumulator, response, index) => {
-        // retrieve all the feature names and put them into an array
-        const allFeatureNames = response.data.outputs.map((feature) => feature.name);
-        // store the name and features under the dataset id
-        accumulator[selectedDatasets[index]] = {
-          name: response.data.name, features: allFeatureNames
-        };
-        return accumulator;
-      }, {});
-
-      // and send it to redux
-      dispatch(setSavedDatasets(parsedDatasets));
-      dispatch(nextModelerStep());
-    });
-  };
-
-  const handleRowSelection = (rows) => {
-    setSelectedDatasets(rows);
-  };
 
   if (datasetsLoading) {
-    return <h3>Loading...</h3>;
+    return <h3>Loading...</h3>
   }
 
   if (datasetsError) {
-    return <h3>Error</h3>;
+    return <h3>Error</h3>
   }
-
+console.log('datasets', datasets)
   return (
-    <Container maxWidth="lg">
-      <Typography align="center" sx={{ marginTop: 4 }} gutterBottom variant="h4">
-        Data Modeler
-      </Typography>
-      <Typography align="center" sx={{ marginBottom: 4 }} variant="subtitle1">
-        Select any number of netcdf datasets from the table below to load into the data modeler
-      </Typography>
-      <DataGrid
-        checkboxSelection
-        autoHeight
-        columns={columns}
-        rows={datasets}
-        onSelectionModelChange={handleRowSelection}
-      />
-      <Button onClick={handleNext} sx={{ marginY: 1 }} disabled={!selectedDatasets.length}>
-        Use these datasets
-      </Button>
-    </Container>
+    <div>
+      {datasets.map((dataset) => (
+        <div>
+          <h4>This is the dataset: {dataset.name}</h4>
+          <span>{`${dataset.id} | ${dataset.fileData.raw.url}`}</span>
+        </div>
+      ))}
+    </div>
   );
 };
-
+// id, name, description, created_at, maintainer, fileData, deprecated
 export default DagDatasetSelector;
