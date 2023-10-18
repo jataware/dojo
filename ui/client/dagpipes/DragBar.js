6a1dd5b124387f239feff7a92f2adb77a75cbca4
Adam Gilgoff <adam.gilg@gmail.com>
2023-10-18 16:13:44 -0400

    Cleans up drag buttons

--- ui/client/dagpipes/DragBar.js
+++ ui/client/dagpipes/DragBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
 import { makeStyles } from 'tss-react/mui';
@@ -13,13 +15,36 @@ const useStyles = makeStyles()((theme) => ({
     gap: theme.spacing(2),
     margin: theme.spacing(2),
     '& > button': {
-      fontSize: '10px',
-      padding: '0.5rem 3.5rem',
-      cursor: 'grab',
+      justifyContent: 'flex-start',
+      gap: 0,
+      padding: `${theme.spacing(1)} ${theme.spacing(2)}`,
+      cursor: 'default',
       maxWidth: '200px',
     }
   },
 }));
+
+const DragButton = ({
+  label, name, onDragStart, green
+}) => (
+  <Button
+    color={green ? 'success' : 'primary'}
+    variant="outlined"
+    startIcon={<DragIndicatorIcon sx={{ cursor: 'grab' }} />}
+    disableRipple
+    onDragStart={(event) => onDragStart(event, name)}
+    draggable
+    sx={{
+      '& .MuiButton-startIcon': {
+        marginRight: 0,
+      },
+    }}
+  >
+    <Typography variant="caption" sx={{ cursor: 'grab' }}>
+      &nbsp;{label}
+    </Typography>
+  </Button>
+);
 
 export default () => {
   const { classes } = useStyles();
@@ -31,67 +56,16 @@ export default () => {
 
   return (
     <div className={classes.sidebar}>
-      <Button
-        color="success"
-        variant="outlined"
-        startIcon={<DragIndicatorIcon />}
-        disableRipple
-        onDragStart={(event) => onDragStart(event, 'load')}
-        draggable
-      >
-        {NodeTitles.LOAD}
-      </Button>
-
-      <Button
-        variant="outlined"
-        startIcon={<DragIndicatorIcon />}
-        disableRipple
-        onDragStart={(event) => onDragStart(event, 'threshold')}
-        draggable
-      >
-        {NodeTitles.THRESHOLD}
-      </Button>
-
-      <Button
-        variant="outlined"
-        startIcon={<DragIndicatorIcon />}
-        disableRipple
-        onDragStart={(event) => onDragStart(event, 'multiply')}
-        draggable
-      >
-        {NodeTitles.MULTIPLY}
-      </Button>
-
-      <Button
-        variant="outlined"
-        startIcon={<DragIndicatorIcon />}
-        disableRipple
-        onDragStart={(event) => onDragStart(event, 'filter_by_country')}
-        draggable
-      >
-        {NodeTitles.FILTER_BY_COUNTRY}
-      </Button>
-
-      <Button
-        variant="outlined"
-        startIcon={<DragIndicatorIcon />}
-        disableRipple
-        onDragStart={(event) => onDragStart(event, 'sum')}
-        draggable
-      >
-        {NodeTitles.SUM}
-      </Button>
-
-      <Button
-        color="success"
-        variant="outlined"
-        startIcon={<DragIndicatorIcon />}
-        disableRipple
-        onDragStart={(event) => onDragStart(event, 'save')}
-        draggable
-      >
-        {NodeTitles.SAVE}
-      </Button>
+      <DragButton label={NodeTitles.LOAD} name="load" onDragStart={onDragStart} green />
+      <DragButton label={NodeTitles.THRESHOLD} name="threshold" onDragStart={onDragStart} />
+      <DragButton label={NodeTitles.MULTIPLY} name="multiply" onDragStart={onDragStart} />
+      <DragButton
+        label={NodeTitles.FILTER_BY_COUNTRY}
+        name="filter_by_country"
+        onDragStart={onDragStart}
+      />
+      <DragButton label={NodeTitles.SUM} name="sum" onDragStart={onDragStart} />
+      <DragButton label={NodeTitles.SAVE} name="save" onDragStart={onDragStart} green />
     </div>
   );
 };
