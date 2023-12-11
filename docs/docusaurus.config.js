0778cde218d589ef9f8afc324afd6d4a874e4af0
adamgilg <adam.gilg@gmail.com>
2023-12-11 09:38:29 -0800

    Fixes docs baseurl (#215)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://www.dojo-modeling.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/dojo/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
