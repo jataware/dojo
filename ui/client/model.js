e12ea9f19a894c325d216940d333920c91ef408f
Adam Gilgoff <adam.gilg@gmail.com>
2024-01-08 19:00:52 -0500

    Lint fixes - removing now unnecessary useEffects

--- ui/client/model.js
+++ ui/client/model.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
--- ui/client/runlogs.js
+++ ui/client/runlogs.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
