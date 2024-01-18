aea9b3cdfaee22506cab7444114a010b01e9437f
Adam Gilgoff <adam.gilg@gmail.com>
2024-01-18 11:12:32 -0500

    Adds initial Data Modeling error handling with unstyled error card

--- ui/client/dagpipes/ModelerProcessing.js
+++ ui/client/dagpipes/ModelerProcessing.js
@@ -1,9 +1,13 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import axios from 'axios';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import CircularProgress from '@mui/material/CircularProgress';
@@ -12,7 +16,7 @@ import { setCompletedDatasetIds, nextModelerStep } from './dagSlice';
 import { ThemeContext } from '../components/ThemeContextProvider';
 
 // TODO: need to ensure it has some way to cancel job if the component unmounts
-const repeatFetch = async ({ jobId, onSuccess }) => {
+const repeatFetch = async ({ jobId, onSuccess, onFailure }) => {
   let shouldContinue = true;
 
   if (!jobId) return;
@@ -28,6 +32,7 @@ const repeatFetch = async ({ jobId, onSuccess }) => {
       }
     } catch (error) {
       console.log(`There was an error fetching job ${jobId}: ${error}`);
+      onFailure(error);
       shouldContinue = false;
     }
 
@@ -39,9 +44,21 @@ const repeatFetch = async ({ jobId, onSuccess }) => {
   }
 };
 
+const ErrorCard = () => (
+  <Card>
+    <CardContent>
+      <Typography variant="h6">There was a problem processing your graph</Typography>
+    </CardContent>
+    <CardActions>
+      <Button>Go Back</Button>
+    </CardActions>
+  </Card>
+);
+
 const ModelerProcessing = () => {
   const { flowcastJobId } = useSelector((state) => state.dag);
   const dispatch = useDispatch();
+  const [showError, setShowError] = useState(false);
 
   const { setShowSideBar } = useContext(ThemeContext);
 
@@ -55,16 +72,21 @@ const ModelerProcessing = () => {
   useEffect(() => {
     const onSuccess = (resp) => {
       console.log('Successful!', resp);
-      if (resp.results) {
+      if (resp.hasOwnProperty('error')) {
+        setShowError(true);
+      } else if (resp.results) {
         const derivedDatasets = resp.results.map((dataset) => dataset.id);
         dispatch(setCompletedDatasetIds(derivedDatasets));
         dispatch(nextModelerStep());
       }
-      // TODO: handle errors - task needs to be modified to return an actual error
+    };
+
+    const onFailure = () => {
+      setShowError(true);
     };
 
     console.log('this is the jobId', flowcastJobId);
-    repeatFetch({ jobId: flowcastJobId, onSuccess });
+    repeatFetch({ jobId: flowcastJobId, onSuccess, onFailure });
   }, [flowcastJobId, dispatch]);
 
   return (
@@ -81,8 +103,14 @@ const ModelerProcessing = () => {
         marginTop: 18,
       }}
     >
-      <Typography variant="h4">Processing...</Typography>
-      <CircularProgress size={38} color="inherit" />
+      {showError ? (
+        <ErrorCard />
+      ) : (
+        <>
+          <Typography variant="h4">Processing...</Typography>
+          <CircularProgress size={38} color="inherit" />
+        </>
+      )}
     </Container>
   );
 };
